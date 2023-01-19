189f53fc826972968c42983aa235d6f5bd74bb00
reyhansaini123 <123072195+reyhansaini123@users.noreply.github.com>
2023-01-19 15:53:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+function preload(){
+
+
+
+}
+
+function setup(){
+
+canvas = createCanvas(400,400);
+ellipse(60,80, 55, 55);
+rect(10, 80, 30, 250);
+rect(60, 50, 250, 30);
+circle(300, 300, 200);
+rect(300, 300, 300, 300);
+
+}
+
+function draw(){
+Image(video,0,0,400,400);
+
+
+
+}
+
+function take_snapshot(){
+
+
+save('now.png');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
